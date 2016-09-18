21587dd47d667755dc6e5c53421b06b52258ace9
arpchauh <arpitchauhan10@gmail.com>
2016-09-18 11:31:25 +0530

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,17 @@
+package CoeJavaSession1;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		a= a+b;
+		b= a-b;
+		a=a-b;
+		System.out.println("After swaping the value of a is : "+a+"\n and the value of b is : "+b);
+
+	}
+
+}
